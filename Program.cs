e06d0036487f6aa37d5f5f4846713e10a0136c6c
Samuel Karlström <sam.karlstrm@gmail.com>
2020-11-05 17:09:51 +0100

    Storted out Main, added methods(PrintMenu and GetUserInput)

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
+using System.Xml.Serialization;
 
 namespace progmet_assignment1
 {
@@ -47,7 +49,7 @@ namespace progmet_assignment1
             }
             public string GetPersonString()
             {
-                return $"{name}, {phone}, {email}, {address}";
+                return $"{name},{phone},{email},{address}";
             }
             public void PrintPerson()
             {
@@ -60,8 +62,12 @@ namespace progmet_assignment1
 
         static void Main(string[] args)
         {
-            //reads from file (du får byta till tomki :) )
+            string usrInpt;
+            List<Person> addressbook = new List<Person>();
+            //reads from this file (du får byta till tomki :) )
             string file_name = "C:\\Users\\samka\\addressbook.txt";
+            //saves to this file after quitting program
+            string new_file_name = "C:\\Users\\samka\\newaddressbook.txt";
 
             string[] lines = { };
             if (File.Exists(file_name))
@@ -70,10 +76,11 @@ namespace progmet_assignment1
             }
             else
             {
-                Console.WriteLine("No file found called {0}",file_name);
+                Console.WriteLine("No input-file found called {0}", file_name);
+                Console.WriteLine("press any key to exit");
+                Console.ReadKey();
+                return;
             }
-
-            List<Person> addressbook = new List<Person>();
 
             foreach (string line in lines)
             {
@@ -82,36 +89,83 @@ namespace progmet_assignment1
                 addressbook.Add(new Person(personStrings));
             }
 
-            foreach (Person P in addressbook)
-                P.PrintPerson();
+            
+
+            while (true)
+            {
+                PrintMenu();
+
+                usrInpt = GetUserInput();
+
+                if (usrInpt == "0")
+                    break;
+
+                if (usrInpt == "1")
+                {
+                    Console.Clear();
+                    foreach (Person P in addressbook)
+                        P.PrintPerson();
+
+                    Console.WriteLine("Press any key to go back to menu.");
+                    Console.ReadKey();
+                }
+
+
+                
+            }
 
 
 
-
-
-            //print/svae to new file
-            SaveToFile(addressbook);
+            //print/save to new file
+            SaveToFile(new_file_name ,addressbook);
+            Console.WriteLine("Saved info to new file. ({0})",new_file_name);
 
             Console.WriteLine("press any key to exit");
             Console.ReadKey();
         }
 
-        static void SaveToFile(List<Person> a)
+        static void PrintMenu()
         {
-            string new_file_name = "C:\\Users\\samka\\newaddressbook.txt";
-            //open file/create file/overwrite
-            if (!File.Exists(new_file_name))
+            Console.Clear();
+            Console.WriteLine("**********Welcome to the addressbook********");
+            Console.WriteLine("1: Show people.");
+            Console.WriteLine("2: Add Person.");
+            Console.WriteLine("3: Remove Person.");
+            Console.WriteLine("0: Exit addressbook.");
+        }
+        static string GetUserInput()
+        {
+            Console.Write("Enter choice: ");
+            string userInput = Console.ReadLine();
+
+            while ( true )
             {
-                using FileStream fs = File.Create(new_file_name);
-                
+                if (userInput == "1" || userInput == "2" || userInput == "3" || userInput == "0")
+                {
+                    break;
+                }
+
+                Console.WriteLine("Wrong input, please try again: ");
+                userInput = Console.ReadLine();
             }
-            //overwrite
-            File.WriteAllText(new_file_name, "");
+
+            return userInput;
+        }
+        static void SaveToFile(string new_file_name, List<Person> a)
+        {
+            
+            //open file/create file/overwrite
+
+            FileStream fs = File.Create(new_file_name);
+            //FileStream fs = new FileStream(new_file_name,FileMode.Create); är det någon skillnad?
+
             //write to file
             foreach (Person P in a)
             {
-                File.AppendAllText(new_file_name, $"{P.GetPersonString()}\n");
+                fs.Write(Encoding.UTF8.GetBytes($"{P.GetPersonString()}\n"));
+                //File.AppendAllText(new_file_name, $"{P.GetPersonString()}\n");
             }
+            fs.Close();
 
             //loop thorugh Person[] a;
 

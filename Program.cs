92524bbf6697e79431318aae884b3aeb3d0182c6
Samuel Karlström <sam.karlstrm@gmail.com>
2020-11-05 11:47:56 +0100

    added string-array constructor base SaveToFile method

--- Program.cs
+++ Program.cs
@@ -7,10 +7,10 @@ namespace progmet_assignment1
     {
         class Person
         {
-            private string name;
-            private string phone;
-            private string email;
-            private string address;
+            private string name = "";
+            private string phone = "";
+            private string email = "";
+            private string address = "";
 
             public Person(string name, string phone, string email, string address)
             {
@@ -18,6 +18,15 @@ namespace progmet_assignment1
                 this.phone = phone;
                 this.email = email;
                 this.address = address;
+            }
+            public Person(string[] s)
+            {
+
+                name = s[0];
+                phone = s[1];
+                email = s[2];
+                address = s[3];
+                
             }
             public string GetName()
             {
@@ -40,29 +49,51 @@ namespace progmet_assignment1
             {
                 Console.WriteLine("name = {0}, phone = {1}, email = {2}, address = {3}", GetName(), GetPhone(), GetEmail(), GetAddress());
             }
+
+            
         }
 
 
         static void Main(string[] args)
         {
+            //reads from file
             string file_name = "C:\\Users\\samka\\source\\repos\\addressbook.txt";
             
             string[] lines = File.ReadAllLines(file_name);
 
             Person[] addressbook = new Person[lines.Length];
 
+            int i = 0;
             foreach (string line in lines)
             {
-                string[] person = line.Split(' ');
-                Console.WriteLine(person[0] + person[1]);
+                //always has 4 strings (depending on input file??)
+                string[] personStrings = line.Split(',');
+                addressbook[i++] = new Person(personStrings);
+                //addressbook[i++] = new Person(person[0], person[1], person[2], person[3]);
             }
 
-           
+            foreach (Person P in addressbook)
+                P.PrintPerson();
+
+            //print/svae to new file
+            SaveToFile(addressbook);
+
 
 
 
 
             Console.ReadKey();
+        }
+
+        static void SaveToFile(Person[] a)
+        {
+            //open file/create file/overwrite
+
+            //write to file
+
+                //loop thorugh Person[] a;
+
+            //close file
         }
     }
 }
